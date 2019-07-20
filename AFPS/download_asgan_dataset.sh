dc99d4c679bd21782de4393eea52c429675adc4e
Ha0Tang <haotang@pku.edu.cn>
2019-07-21 00:18:28 +0200

    fix typo

--- AFPS/download_asgan_dataset.sh
+++ AFPS/download_asgan_dataset.sh
@@ -8,7 +8,7 @@ fi
 
 echo "Specified [$FILE]"
 
-URL=http://disi.unitn.it/~hao.tang/dataset/ASGAN/$FILE.tar.gz
+URL=http://disi.unitn.it/~hao.tang/uploads/datasets/ASGAN/$FILE.tar.gz
 TAR_FILE=./AFPS/$FILE.tar.gz
 TARGET_DIR=./AFPS/$FILE/
 wget -N $URL -O $TAR_FILE
